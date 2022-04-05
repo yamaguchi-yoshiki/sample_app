a20c1904b88aa6bcad1671b782fcb8b7a613b6c3
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-04-05 10:28:53 +0000

    [Finish] 3-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
+  # top
+  get 'top' => 'homes#top'
+
+  # 新規投稿
   get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => "homes#top"
+  post 'lists' => 'lists#create'
+
+  # 一覧
+  get 'lists' => 'lists#index'
+
+  # 詳細
+  get 'lists/:id' => 'lists#show', as: 'list'
+
+  # 編集
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
